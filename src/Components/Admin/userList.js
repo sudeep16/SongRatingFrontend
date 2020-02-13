02dd2205aad53733c4acd76391243e9add983ee1
sudeep16 <srshakya5@gmail.com>
2020-02-13 12:12:00 +0545

    UsersList Complete

--- src/Components/Admin/userList.js
+++ src/Components/Admin/userList.js
@@ -1,69 +1,91 @@
 import React, { Component } from "react";
-import userHeader from "./adminHeader";
+import adminHeader from "./adminHeader";
 import './admin.css';
 import Axios from "axios";
 import { Route } from "react-router-dom";
-import { Table } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 
 class userList extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             usersList: [],
             config: {
-                headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` }
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                }
             },
             allUsers: {}
         }
     }
 
-    componentDidMount() {
-        Axios.get("http://localhost:2020/users/profile", this.state.config)
+    deleteHandler = (uID) => {
+        const displayUser = this.state.usersList.filter((users) => {
+            return users._id !== uID
+        }) 
+        this.setState({
+            usersList: displayUser
+        })
+
+        Axios.delete(
+            `http://localhost:2020/admin/userList/${uID}`, this.state.config)
             .then((response) => {
-                this.setState({
-                    usersList: response.data
-                });
+                console.log(response)
             })
             .catch((err) => {
                 console.log(err)
             })
     }
+    componentDidMount() {
+        Axios.get("http://localhost:2020/admin/userList", this.state.config)
+            .then((response) => {
+                this.setState({
+                    usersList: response.data
+                });
+                console.log(response.data);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
     render() {
         return (
             <React.Fragment>
-                <Route component={userHeader} />
+                <Route component={adminHeader} />
                 <div>
                     <center><h1>Songs List</h1></center>
+
                     <Table>
                         <thead>
                             <tr>
-                                <th>ID</th>
-                                <th>Song Title</th>
-                                <th>Duration</th>
-                                <th> Genre</th>
-                                <th>Artist</th>
-                                <th>Edit</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Address</th>
+                                <th>Gender</th>
                                 <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                this.state.songs.map((song) => {
-                                    return (    
-                                        <tr key={usersList._id}>
-                                            <td>{usersList.SongTitle}</td>
-                                            <td>{usersList.Duration}</td>
-                                            <td>{usersList.Genre}</td>
-                                            <td>{usersList.Artis}</td>
-                                            <button type="submit">Edit</button>
-                                            <button type="submit">Delete</button>
+                                this.state.usersList.map((users) => {
+                                    return (
+                                        <tr key={users._id}>
+                                            <td>{users.Username}</td>
+                                            <td>{users.Email}</td>
+                                            <td>{users.Phone}</td>
+                                            <td>{users.Address}</td>
+                                            <td>{users.Gender}</td>
+                                            <td><Button onClick = {() => this.deleteHandler(users._id)}>Delete</Button></td>
                                         </tr>
                                     )
                                 })
                             }
                         </tbody>
                     </Table>
+
                 </div>
             </React.Fragment>
         )
